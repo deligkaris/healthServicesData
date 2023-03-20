b07da8aaed14887f357c0ce879527b13ae00ab6c
Christos Deligkaris <deligkaris@users.noreply.github.com>
2023-03-20 16:57:27 -0400

    updated functions

--- cms/base.py
+++ cms/base.py
@@ -75,6 +75,37 @@ def add_through_date_info(baseDF):
 
     return baseDF
 
+def add_discharge_date_info(baseDF):
+
+    baseDF = baseDF.withColumn( "DSCHRGDT_DAYOFYEAR",
+                                F.date_format(
+                                    #THRU_DT was read as bigint, need to convert it to string that can be understood by date_format
+                                    F.concat_ws('-',F.col("DSCHRGDT").substr(1,4),F.col("DSCHRGDT").substr(5,2),F.col("DSCHRGDT").substr(7,2)),
+                                    "D" #get the day of the year
+                                ).cast('int'))
+
+    # keep the claim through year too
+    baseDF = baseDF.withColumn( "DSCHRGDT_YEAR", F.col("DSCHRGDT").substr(1,4).cast('int'))
+
+    # find number of days from yearStart-1 to year of admission -1
+    baseDF = baseDF.withColumn( "DSCHRGDT_DAYSINYEARSPRIOR",
+                                #some admissions have started in yearStart-1
+                                F.when(F.col("DSCHRGDT_YEAR")==2015 ,0)  #this should be yearStart-1
+                                 .when(F.col("DSCHRGDT_YEAR")==2016 ,365)
+                                 .when(F.col("DSCHRGDT_YEAR")==2017 ,366+365) #set them to 366 for leap years
+                                 .when(F.col("DSCHRGDT_YEAR")==2018 ,366+365*2)
+                                 .when(F.col("DSCHRGDT_YEAR")==2019 ,366+365*3)
+                                 .when(F.col("DSCHRGDT_YEAR")==2020 ,366+365*4)
+                                 .when(F.col("DSCHRGDT_YEAR")==2021 ,366*2+365*4)
+                                 .otherwise(365)) #otherwise 365
+
+    # assign a day number starting at day 1 of yearStart-1
+    baseDF = baseDF.withColumn( "DSCHRGDT_DAY",
+                                 # days in years prior to admission + days in year of admission = day nunber
+                                 (F.col("DSCHRGDT_DAYSINYEARSPRIOR") + F.col("DSCHRGDT_DAYOFYEAR")).cast('int'))
+
+    return baseDF
+
 def add_stroke(baseDF):
 
     # PRNCPAL_DGNS_CD: diagnosis, condition problem or other reason for the admission/encounter/visit to 
