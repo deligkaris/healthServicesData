cb57abb118f6d467d3b9ab2e3bead6cbefc46932
Christos Deligkaris <deligkaris@users.noreply.github.com>
2023-04-14 14:08:17 -0400

    add_nihss

--- cms/base.py
+++ cms/base.py
@@ -844,8 +844,8 @@ def add_nihss(baseDF):
                                F.when(
                                    F.size(F.col("nihssList")) == 1, F.substring(F.col("nihssList")[0],5,2))
                                 .otherwise(""))
-                   .withColumn("nihssInts", F.col("nihss").cast('int')))
-                   #.drop("dgnsList","nihssList"))
+                   .withColumn("nihss", F.col("nihss").cast('int')))
+                   .drop("dgnsList","nihssList"))
 
     return baseDF
 
