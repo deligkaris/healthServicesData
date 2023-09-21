1dd5a264acfc5e81ae3ed684a4182e3c2d397bc7
Christos Deligkaris <deligkaris@users.noreply.github.com>
2023-09-21 10:57:49 -0400

    removed first row from carrier files

--- cms/line.py
+++ cms/line.py
@@ -3,7 +3,17 @@ import pyspark.sql.functions as F
 
 def prep_lineDF(lineDF, claim="car"):
 
+    lineDF = clean_line(lineDF, claim=claim)
     lineDF = enforce_schema(lineDF, claim=claim)
+
+    return lineDF
+
+def clean_line(lineDF, claim="car"):
+
+    #mbsf, op, ip files were cleaned and this line was removed in them, but the rest of the files still include the first row
+    #that essentially repeats the column names
+    if ( (claim=="car") ):
+        lineDF = lineDF.filter(~(F.col("DESY_SORT_KEY")=="DESY_SORT_KEY"))
 
     return lineDF
 
