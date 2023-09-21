cfd8f6c376abfc4fff72dbf19f640a484352d4c5
Christos Deligkaris <deligkaris@users.noreply.github.com>
2023-09-21 10:35:07 -0400

    fixed typo

--- cms/line.py
+++ cms/line.py
@@ -12,4 +12,4 @@ def enforce_schema(lineDF, claim="car"):
     if claim=="car":
         lineDF = lineDF.select([ (F.col(field.name).cast(field.dataType)).alias(carLineLongToShortXW[field.name]) for field in carLineSchema.fields])
 
-    return lineeDF
+    return lineDF
