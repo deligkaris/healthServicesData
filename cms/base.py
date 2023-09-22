ce8c52414defaa9dc273227c10d7120bae7320f0
Christos Deligkaris <deligkaris@users.noreply.github.com>
2023-09-22 13:34:31 -0400

    renamed variables in get_aggregate_summary

--- cms/base.py
+++ cms/base.py
@@ -926,7 +926,7 @@ def add_regional_info_from_ers(baseDF,ersPeopleDF, ersJobsDF, ersIncomeDF):
 
      return baseDF
 
-def get_aggregate_summary(baseDF, aggWhat, aggBy = "ssaCounty"): #aggWhat must be an iterable of strings-column names
+def get_aggregate_summary(baseDF, aggItems, aggBy = "ssaCounty"): #aggWhat must be an iterable of strings-column names
 
     baseDF.persist() #since I will use this in a loop make it persist in memory
     baseDF.count()
@@ -935,20 +935,20 @@ def get_aggregate_summary(baseDF, aggWhat, aggBy = "ssaCounty"): #aggWhat must b
 
     baseDF = baseDF.withColumn("total", #find need to find total in unit
                                F.count(F.col(aggBy)).over(eachUnit))
-    returnWhat = [aggBy, "total"]
+    returnItems = [aggBy, "total"]
 
-    for i in aggWhat:
+    for i in aggItems:
         baseDF = baseDF.withColumn(i+"InUnit", #add unit counts
                                    F.sum(
                                        F.col(i)).over(eachUnit))
-        returnWhat = returnWhat + [f'{i}InUnit']
+        returnItems = returnItems + [f'{i}InUnit']
 
         baseDF = baseDF.withColumn(i+"InUnitPerCent", #add unit percentage
                                    F.round(
                                        100.*F.col(i+"InUnit") / F.col("total"),1))
-        returnWhat = returnWhat + [f'{i}InUnitPerCent']
+        returnItems = returnItems + [f'{i}InUnitPerCent']
 
-    aggregateSummary = baseDF.select(returnWhat).distinct() #returnWhat cannot be tuple, list works
+    aggregateSummary = baseDF.select(returnItems).distinct() #returnWhat cannot be tuple, list works
 
     aggregateSummary.persist() #since a loop was involved in calculating this make it persist in memory
     aggregateSummary.count()
