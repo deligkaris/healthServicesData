5fa17eb329709bcbe3e780347dc00fc04859bf6a
Christos Deligkaris <deligkaris@users.noreply.github.com>
2023-09-22 13:25:13 -0400

    modified get_claims

--- cms/claims.py
+++ cms/claims.py
@@ -1,9 +1,8 @@
-def get_claimsDF(baseDF,revenueSummaryDF): #assumes I have already summarized the revenue dataframe
+def get_claimsDF(baseDF,summaryDF): #assumes I have already summarized the revenue dataframe
 
-    # join base and revenue information for each patient and claim
-    claimsDF = baseDF.join(revenueSummaryDF,
-                          on=["DSYSRTKY","CLAIMNO"],
+    #CLAIMNO resets every year so need to include the THRU_DT as well
+    claimsDF = baseDF.join(summaryDF,
+                          on=["DSYSRTKY","CLAIMNO","THRU_DT"],
                           # because we collapsed all revenue records to a single summary revenue record, there should be 1-to-1 match
                           how = "left_outer")
-
     return claimsDF
