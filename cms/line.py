0fbad8557711b2e2e1b9e1f9ae59b7236daf4d91
Christos Deligkaris <deligkaris@users.noreply.github.com>
2023-09-25 06:50:09 -0400

    carrier line functions, some of them, now return summarized results over each claim

--- cms/line.py
+++ cms/line.py
@@ -39,11 +39,8 @@ def add_pcp(lineDF):
 
     pcpCond = 'F.col("HCFASPCL").isin([1,8,11])'
 
-    eachDsysrtkyAndClaim = Window.partitionBy(["DSYSRTKY","CLAIMNO"])
+    lineDF = lineDF.withColumn("pcp", F.when( eval(pcpCond), 1).otherwise(0))
 
-    lineDF = lineDF.withColumn("pcp", 
-                               F.when( eval(pcpCond), 1)
-                                .otherwise(0))
     return lineDF
 
 def add_alfVisit(lineDF): #alf: assisted living facility
@@ -51,9 +48,8 @@ def add_alfVisit(lineDF): #alf: assisted living facility
     alfVisitCond = '''( ((F.col("level1HCPCS_CD")>=99324)&(F.col("level1HCPCS_CD")<=99328)) |
                         ((F.col("level1HCPCS_CD")>=99334)&(F.col("level1HCPCS_CD")<=99338)) )'''
 
-    lineDF = lineDF.withColumn("alfVisit",
-                               F.when( eval(alfVisitCond), 1)
-                                .otherwise(0))
+    lineDF = lineDF.withColumn("alfVisit", F.when( eval(alfVisitCond), 1).otherwise(0))
+
     return lineDF
 
 def add_opVisit(lineDF):
@@ -61,36 +57,32 @@ def add_opVisit(lineDF):
     opVisitCond = '''( ((F.col("level1HCPCS_CD")>=99201)&(F.col("level1HCPCS_CD")<=99205)) |
                      ((F.col("level1HCPCS_CD")>=99211)&(F.col("level1HCPCS_CD")<=99215)) )'''
 
-    lineDF = lineDF.withColumn("opVisit",
-                               F.when( eval(opVisitCond), 1)
-                                .otherwise(0))
+    lineDF = lineDF.withColumn("opVisit", F.when( eval(opVisitCond), 1).otherwise(0))
+
     return lineDF
 
 def add_neurology(lineDF):
 
     neurologyCond = 'F.col("HCFASPCL")==13'
 
-    lineDF = lineDF.withColumn("neurology",
-                               F.when( eval(neurologyCond), 1)
-                                .otherwise(0))
+    lineDF = lineDF.withColumn("neurology", F.when( eval(neurologyCond), 1).otherwise(0))
+
     return lineDF
 
 def add_neuropsychiatry(lineDF):
 
     neuropsychiatryCond = 'F.col("HCFASPCL")==86'
 
-    lineDF = lineDF.withColumn("neuropsychiatry",
-                               F.when( eval(neuropsychiatryCond), 1)
-                                .otherwise(0))
+    lineDF = lineDF.withColumn("neuropsychiatry", F.when( eval(neuropsychiatryCond), 1).otherwise(0))
+
     return lineDF
 
 def add_geriatric(lineDF):
 
     geriatricCond = 'F.col("HCFASPCL")==38'
 
-    lineDF = lineDF.withColumn("geriatric",
-                               F.when( eval(geriatricCond), 1)
-                                .otherwise(0))
+    lineDF = lineDF.withColumn("geriatric", F.when( eval(geriatricCond), 1).otherwise(0))
+
     return lineDF
 
 def add_neurologyOpVisit(lineDF):
@@ -98,8 +90,12 @@ def add_neurologyOpVisit(lineDF):
     lineDF = add_neurology(lineDF)
     lineDF = add_opVisit(lineDF)
 
-    lineDF = lineDF.withColumn("neurologyOpVisit", F.col("neurology")*F.col("opVisit"))
+    #unique claim is found with CLAIMNO and THRU_DT
+    eachDsysrtkyAndClaim = Window.partitionBy(["DSYSRTKY","CLAIMNO","THRU_DT"])
 
+    lineDF = (lineDF.withColumn("neurologyOpVisit", F.col("neurology")*F.col("opVisit"))
+                    .withColumn("neurologyOpVisit", F.max(F.col("neurologyOpVisit")).over(eachDsysrtkyAndClaim)))
+                                
     return lineDF
 
 def add_pcpOpVisit(lineDF):
@@ -107,7 +103,11 @@ def add_pcpOpVisit(lineDF):
     lineDF = add_pcp(lineDF)
     lineDF = add_opVisit(lineDF)
 
-    lineDF = lineDF.withColumn("pcpOpVisit", F.col("pcp")*F.col("opVisit"))
+    #unique claim is found with CLAIMNO and THRU_DT
+    eachDsysrtkyAndClaim = Window.partitionBy(["DSYSRTKY","CLAIMNO","THRU_DT"])
+
+    lineDF = (lineDF.withColumn("pcpOpVisit", F.col("pcp")*F.col("opVisit"))
+                    .withColumn("pcpOpVisit", F.max(F.col("pcpOpVisit")).over(eachDsysrtkyAndClaim))) 
 
     return lineDF
 
@@ -115,27 +115,32 @@ def add_ct(lineDF):
 
     ctCond = 'F.col("level1HCPCS_CD").isin([70450,70460,70470])'
 
-    lineDF = lineDF.withColumn("ct",
-                                F.when( eval(ctCond), 1)
-                                 .otherwise(0))
+    #unique claim is found with CLAIMNO and THRU_DT
+    eachDsysrtkyAndClaim = Window.partitionBy(["DSYSRTKY","CLAIMNO","THRU_DT"])
+
+    lineDF = (lineDF.withColumn("ct", F.when( eval(ctCond), 1).otherwise(0))
+                    .withColumn("ct", F.max(F.col("ct")).over(eachDsysrtkyAndClaim)))
+
     return lineDF
 
 def add_mri(lineDF):
 
     mriCond = 'F.col("level1HCPCS_CD").isin([70551,70553])'
 
-    lineDF = lineDF.withColumn("mri",
-                               F.when( eval(mriCond), 1)
-                                .otherwise(0))
+    #unique claim is found with CLAIMNO and THRU_DT
+    eachDsysrtkyAndClaim = Window.partitionBy(["DSYSRTKY","CLAIMNO","THRU_DT"])
+
+    lineDF = (lineDF.withColumn("mri", F.when( eval(mriCond), 1).otherwise(0))
+                    .withColumn("mri", F.max(F.col("mri")).over(eachDsysrtkyAndClaim)))
+
     return lineDF
 
 def add_homeVisit(lineDF):
 
     homeVisitCond = '((F.col("level1HCPCS_CD")>=99342)&(F.col("level1HCPCS_CD")<=99350))' 
 
-    lineDF = lineDF.withColumn("homeVisit",
-                               F.when( eval(homeVisitCond), 1)
-                                .otherwise(0))
+    lineDF = lineDF.withColumn("homeVisit", F.when( eval(homeVisitCond), 1).otherwise(0))
+
     return lineDF
 
 def add_pcpHomeVisit(lineDF):
@@ -143,7 +148,11 @@ def add_pcpHomeVisit(lineDF):
     lineDF = add_pcp(lineDF)
     lineDF = add_homeVisit(lineDF)
 
-    lineDF = lineDF.withColumn("pcpHomeVisit", F.col("pcp")*F.col("homeVisit"))
+    #unique claim is found with CLAIMNO and THRU_DT
+    eachDsysrtkyAndClaim = Window.partitionBy(["DSYSRTKY","CLAIMNO","THRU_DT"])
+
+    lineDF = (lineDF.withColumn("pcpHomeVisit", F.col("pcp")*F.col("homeVisit"))
+                    .withColumn("pcpHomeVisit", F.max(F.col("pcpHomeVisit")).over(eachDsysrtkyAndClaim)))
 
     return lineDF
 
@@ -152,7 +161,11 @@ def add_pcpAlfVisit(lineDF):
     lineDF = add_pcp(lineDF)
     lineDF = add_alfVisit(lineDF)
 
-    lineDF = lineDF.withColumn("pcpAlfVisit", F.col("pcp")*F.col("alfVisit"))
+    #unique claim is found with CLAIMNO and THRU_DT
+    eachDsysrtkyAndClaim = Window.partitionBy(["DSYSRTKY","CLAIMNO","THRU_DT"])
+
+    lineDF = (lineDF.withColumn("pcpAlfVisit", F.col("pcp")*F.col("alfVisit"))
+                    .withColumn("pcpAlfVisit", F.max(F.col("pcpAlfVisit")).over(eachDsysrtkyAndClaim)))
 
     return lineDF
 
@@ -161,7 +174,11 @@ def add_geriatricOpVisit(lineDF):
     lineDF = add_geriatric(lineDF)
     lineDF = add_opVisit(lineDF)
 
-    lineDF = lineDF.withColumn("geriatricOpVisit", F.col("geriatric")*F.col("opVisit"))
+    #unique claim is found with CLAIMNO and THRU_DT
+    eachDsysrtkyAndClaim = Window.partitionBy(["DSYSRTKY","CLAIMNO","THRU_DT"])
+
+    lineDF = (lineDF.withColumn("geriatricOpVisit", F.col("geriatric")*F.col("opVisit"))
+                    .withColumn("geriatricOpVisit", F.max(F.col("geriatricOpVisit")).over(eachDsysrtkyAndClaim)))
 
     return lineDF
 
@@ -170,7 +187,11 @@ def add_neuropsychiatryOpVisit(lineDF):
     lineDF = add_neuropsychiatry(lineDF)
     lineDF = add_opVisit(lineDF)
 
-    lineDF = lineDF.withColumn("neuropsychiatryOpVisit", F.col("neuropsychiatry")*F.col("opVisit"))
+    #unique claim is found with CLAIMNO and THRU_DT
+    eachDsysrtkyAndClaim = Window.partitionBy(["DSYSRTKY","CLAIMNO","THRU_DT"])
+
+    lineDF = (lineDF.withColumn("neuropsychiatryOpVisit", F.col("neuropsychiatry")*F.col("opVisit"))
+                    .withColumn("neuropsychiatryOpVisit", F.max(F.col("neuropsychiatryOpVisit")).over(eachDsysrtkyAndClaim)))
 
     return lineDF
 
@@ -178,8 +199,8 @@ def filter_claims(lineDF, baseDF):
 
     #CLAIMNO resets every year, so I need CLAIMNO, DSYSRTKY and THRU_DT to uniquely link base and line files
     lineDF = lineDF.join(baseDF.select(F.col("CLAIMNO"),F.col("DSYSRTKY"),F.col("THRU_DT")),
-                               on=["CLAIMNO","DSYSRTKY","THRU_DT"],
-                               how="left_semi")
+                         on=["CLAIMNO","DSYSRTKY","THRU_DT"],
+                         how="left_semi")
 
     return lineDF
 
@@ -187,9 +208,8 @@ def add_allowed(lineDF):
 
     allowedCond = '(F.col("PRCNGIND")=="A")'
      
-    lineDF = lineDF.withColumn("allowed",
-                               F.when( eval(allowedCond), 1)
-                                .otherwise(0))
+    lineDF = lineDF.withColumn("allowed", F.when( eval(allowedCond), 1).otherwise(0))
+
     return lineDF
 
 
