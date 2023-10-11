50e2c0cc19e7f8fbceec0608fdfcd23c95b49e48
Christos Deligkaris <deligkaris@users.noreply.github.com>
2023-10-11 17:20:18 -0400

    turn off schema

--- cms/utilities.py
+++ cms/utilities.py
@@ -91,8 +91,8 @@ def read_and_prep_dataframe(filename, claimTypePart, spark):
 
     df = spark.read.parquet(filename)
     #if DESY_SORT_KEY exists in columns names then mark that as a df that is using the long column names
-    #if ("DESY_SORT_KEY" in df.columns):
-    #    df = enforce_short_names(df, claimType=claimType, claimPart=claimPart)
+    if ("DESY_SORT_KEY" in df.columns):
+        df = enforce_short_names(df, claimType=claimType, claimPart=claimPart)
     #enforce the schema now, dataframes need to have the same schema before doing the unions
     #in some dataframes the first row is a copy of the header, enforce_schema has made the "DSYSRTKY" string of that first row a null value
     #so I need to remove that row, assumes that the DSYSRTKY col is cast to an int in schema.py
