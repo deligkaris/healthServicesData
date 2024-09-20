72c9d986edb4ad988feab308031a7ba4323ee6de
Christos Deligkaris <deligkaris@users.noreply.github.com>
2024-09-20 12:58:31 -0400

    bring some info from base to revenue when filtering revenue

--- cms/revenue.py
+++ cms/revenue.py
@@ -78,7 +78,7 @@ def add_echo(revenueDF, inClaim=False):
 
 def filter_claims(revenueDF, baseDF):
     #CLAIMNO resets every year, so I need CLAIMNO, DSYSRTKY and THRU_DT to uniquely link base and revenue files
-    revenueDF = revenueDF.join(baseDF.select(F.col("CLAIMNO"),F.col("DSYSRTKY"),F.col("THRU_DT")),
+    revenueDF = revenueDF.join(baseDF.select(F.col("CLAIMNO"),F.col("DSYSRTKY"),F.col("THRU_DT"),F.col("ORGNPINM"),F.col("THRU_DT_YEAR")),
                                on=["CLAIMNO","DSYSRTKY","THRU_DT"],
                                how="left_semi")
     return revenueDF
