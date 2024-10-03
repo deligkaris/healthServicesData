ee5ed9c67baffdf47f9f2813d456473a5aa2e0bc
Christos Deligkaris <deligkaris@users.noreply.github.com>
2024-10-03 13:30:12 -0400

    in admision date info added month info

--- cms/base.py
+++ cms/base.py
@@ -3,7 +3,7 @@ from pyspark.sql.window import Window
 from .mbsf import add_ohResident
 from .revenue import filter_claims, add_ed, add_mri, add_ct, get_revenue_info
 from .claims import get_claimsDF
-from utilities import add_primaryTaxonomy, add_acgmeSitesInZip, add_acgmeProgramsInZip, daysInYearsPrior, usRegionFipsCodes
+from utilities import add_primaryTaxonomy, add_acgmeSitesInZip, add_acgmeProgramsInZip, daysInYearsPrior, usRegionFipsCodes, monthsInYearsPrior
 import re
 from functools import reduce
 
