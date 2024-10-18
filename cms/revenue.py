f91eed88de7f3517f92a4bef03c0b7ca8153df13
Christos Deligkaris <deligkaris@users.noreply.github.com>
2024-10-18 11:29:21 -0400

    updated everything in revenue to be based on thru day

--- cms/revenue.py
+++ cms/revenue.py
@@ -11,7 +11,7 @@ def add_ed(revenueDF, inClaim=False):
     # Claims in the Outpatient and Inpatient files are identified via Revenue Center Code 
     # values of 0450-0459 (Emergency room) or 0981 (Professional fees-Emergency room).
     if (inClaim):
-        eachClaim = Window.partitionBy(["DSYSRTKY","CLAIMNO","THRU_DT"])
+        eachClaim = Window.partitionBy(["DSYSRTKY","CLAIMNO","THRU_DT_DAY"])
         revenueDF = (revenueDF.withColumn("ed",
                                           F.when( (F.col("REV_CNTR")>= 450) & (F.col("REV_CNTR") <= 459) ,1)
                                            .when( F.col("REV_CNTR")==981 ,1)
@@ -26,7 +26,7 @@ def add_ed(revenueDF, inClaim=False):
 
 def add_mri(revenueDF, inClaim=False):
     if (inClaim):
-        eachClaim = Window.partitionBy(["DSYSRTKY","CLAIMNO","THRU_DT"])
+        eachClaim = Window.partitionBy(["DSYSRTKY","CLAIMNO","THRU_DT_DAY"])
         revenueDF = (revenueDF.withColumn("mri", 
                                           F.when( (F.col("REV_CNTR")>= 610) & (F.col("REV_CNTR") <= 619) ,1)
                                            .otherwise(0))
@@ -39,7 +39,7 @@ def add_mri(revenueDF, inClaim=False):
 
 def add_ct(revenueDF, inClaim=False):
     if (inClaim):
-        eachClaim = Window.partitionBy(["DSYSRTKY","CLAIMNO","THRU_DT"]) 
+        eachClaim = Window.partitionBy(["DSYSRTKY","CLAIMNO","THRU_DT_DAY"]) 
         revenueDF = (revenueDF.withColumn("ct",
                                           F.when( (F.col("REV_CNTR")>= 350) & (F.col("REV_CNTR") <= 359) ,1)
                                            .otherwise(0))
@@ -54,7 +54,7 @@ def add_echo(revenueDF, inClaim=False):
     echoCodes = ["93304", "93306", "93307", "93320", "93321", "93312", "93313", "93314","93315", "93316", "93317"]
     echoCondition = '(F.col("HCPCS_CD").isin(echoCodes))'
     if (inClaim):
-        eachClaim = Window.partitionBy(["DSYSRTKY","CLAIMNO","THRU_DT"])
+        eachClaim = Window.partitionBy(["DSYSRTKY","CLAIMNO","THRU_DT_DAY"])
         revenueDF = (revenueDF.withColumn("echo",
                                           F.when( eval(echoCondition) ,1)
                                            .otherwise(0))
@@ -67,7 +67,7 @@ def add_echo(revenueDF, inClaim=False):
 
 def filter_claims(revenueDF, baseDF):
     #CLAIMNO resets every year, so I need CLAIMNO, DSYSRTKY and THRU_DT to uniquely link base and revenue files
-    revenueDF = revenueDF.join(baseDF.select(F.col("CLAIMNO"),F.col("DSYSRTKY"),F.col("THRU_DT")),
+    revenueDF = revenueDF.join(baseDF.select(F.col("CLAIMNO"),F.col("DSYSRTKY"),F.col("THRU_DT_DAY")),
                                on=["CLAIMNO","DSYSRTKY","THRU_DT"],
                                how="left_semi")
     return revenueDF
@@ -75,7 +75,7 @@ def filter_claims(revenueDF, baseDF):
 def get_revenue_summary(revenueDF):
     #the only summary I think I typically need are the inClaim summaries, and those columns now end with InClaim
     #include a few other columns so that I can link the summary to the base 
-    revenueSummaryDF = revenueDF.select("DSYSRTKY", "CLAIMNO", "THRU_DT", revenueDF.colRegex("`^[a-zA-Z]+(InClaim)$`")).distinct()
+    revenueSummaryDF = revenueDF.select("DSYSRTKY", "CLAIMNO", "THRU_DT_DAY", revenueDF.colRegex("`^[a-zA-Z]+(InClaim)$`")).distinct()
     #the summary will be joined to base, so the InClaim is no longer needed
     namesWithoutInClaim = [re.sub("InClaim","",x) for x in revenueSummaryDF.columns]
     revenueSummaryDF = revenueSummaryDF.toDF(*namesWithoutInClaim)
