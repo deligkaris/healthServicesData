d1dd79d788a5d5176b889cb68495daae5b54c2b9
Christos Deligkaris <deligkaris@users.noreply.github.com>
2024-10-18 12:34:20 -0400

    undid effectiveness changes in claims.py

--- cms/claims.py
+++ cms/claims.py
@@ -41,7 +41,9 @@ def get_unique_stays(claimsDF, claimType="op"):
     eachIpStay = Window.partitionBy("DSYSRTKY","PROVIDER", "ORGNPINM", "ADMSN_DT_DAY", "DSCHRGDT_DAY")
     eachOpStay = Window.partitionBy("DSYSRTKY","PROVIDER", "ORGNPINM", "THRU_DT_DAY")
     eachStay = eachIpStay if claimType=="ip" else eachOpStay
-    claimsDF = claimsDF.filter( F.min(F.col("CLAIMNO")).over(eachStay) == F.col("CLAIMNO") )
+    claimsDF = (claimsDF.withColumn("minClaimnoForStay", F.min(F.col("CLAIMNO")).over(eachStay) )
+                        .filter( F.col("minClaimnoForStay") == F.col("CLAIMNO") )
+                        .drop("minClaimnoForStay"))
     return claimsDF
 
 
