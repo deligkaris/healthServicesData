1191b97802b85abf6f1d650597738c1a8799d600
Christos Deligkaris <deligkaris@users.noreply.github.com>
2024-10-18 12:14:38 -0400

    made mistake, revenue does not have thru day info

--- cms/claims.py
+++ cms/claims.py
@@ -4,7 +4,7 @@ import pyspark.sql.functions as F
 def get_claimsDF(baseDF,summaryDF): #assumes I have already summarized the revenue dataframe
     #CLAIMNO resets every year so need to include the THRU_DT as well
     claimsDF = baseDF.join(summaryDF,
-                          on=["DSYSRTKY","CLAIMNO","THRU_DT_DAY"],
+                          on=["DSYSRTKY","CLAIMNO","THRU_DT"],
                           # because we collapsed all revenue records to a single summary revenue record, there should be 1-to-1 match
                           how = "left_outer")
     claimsDF = add_provider_revenue_info(claimsDF)
