985266ecf82468c9fdf7d628a0905844e3345580
Christos Deligkaris <deligkaris@users.noreply.github.com>
2024-11-27 07:50:51 -0500

    add_providerIn50StatesOrDc in base

--- cms/base.py
+++ cms/base.py
@@ -217,7 +217,6 @@ def add_dbsCpt(baseDF):
     return baseDF
 
 def add_ohProvider(baseDF):
-
     # keep providers in OH (PRSTATE)
     # ohio is code 36, SSA code, https://resdac.org/cms-data/variables/state-code-claim-ssa
     ohProviderCondition = '(F.col("PRSTATE")=="36")'
@@ -397,7 +396,7 @@ def add_ccnCah(baseDF):
                                           .otherwise(0))
     return baseDF
 
-def add_providerFIPSToNulls(baseDF,posDF,zipToCountyDF): #assumes add_providerCounty
+#def add_providerFIPSToNulls(baseDF,posDF,zipToCountyDF): #assumes add_providerCounty
 
     #for the rest 0.1% I will use a probabilistic method to get the fips county, but even with this method there will still be some nulls
 
@@ -415,6 +414,13 @@ def add_providerFIPSToNulls(baseDF,posDF,zipToCountyDF): #assumes add_providerCo
 
     #baseDF = baseDF.drop("providerFIPSzipToCounty")
 
+    #return baseDF
+
+def add_providerIn50StatesOrDc(baseDF):
+    '''The SSA state code for provider or facility is one of the 50 US states or DC.
+    SSA code 48 = Virgin Islands, SSA code 40 = Puerto Rico.'''
+    condition = ' (F.col("PRSTATE").cast('int') < 54) & (F.col("PRSTATE").cast('int') != 40) & (F.col("PRSTATE").cast('int') != 48) '
+    baseDF = baseDF.withColumn("providerIn50StatesOrDc", F.when( eval(condition), 1).otherwise(0))
     return baseDF
 
 def add_provider_pos_info(baseDF, posDF):
@@ -464,6 +470,7 @@ def add_provider_info(baseDF, data):
     baseDF = add_aha_info(baseDF, data["aha"])
     baseDF = add_providerSysId(baseDF, data["chspHosp"])
     baseDF = add_providerCmi(baseDF, data["cmi"])
+    baseDF = add_providerIn50StatesOrDc(baseDF)
     return baseDF
 
 def add_osu(baseDF):
