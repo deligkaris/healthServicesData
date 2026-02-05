701b151923f2f65c9255bed7bcada0abe37b035a
Christos Deligkaris <deligkaris@users.noreply.github.com>
2026-02-05 09:51:19 -0500

    modified septic shock poa function

--- cms/base.py
+++ cms/base.py
@@ -247,7 +247,9 @@ def add_septicShockPoa(baseDF):
     Reference: https://journals.lww.com/ccmjournal/abstract/2019/04000/variation_in_identifying_sepsis_and_organ.1.aspx
     Reference: https://journals.lww.com/lww-medicalcare/abstract/2014/06000/identifying_patients_with_severe_sepsis_using.18.aspx'''
     septicShockDgnsCodes = ("R6521",)
-    baseDF = baseDF.withColumn( "septicShockPoa", F.array_intersect(F.col("dgnsPoaCodeAll"), F.array([F.lit(c) for c in septicShockDgnsCodes])))
+    baseDF = baseDF.withColumn( "septicShockPoa", 
+                                F.when( F.array_intersect(F.col("dgnsPoaCodeAll"), F.array([F.lit(c) for c in septicShockDgnsCodes])), F.lit(1))
+                                 .otherwise(F.lit(0)))
     return baseDF
 
 def add_parkinsonsPrncpalDgns(baseDF):
