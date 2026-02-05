4a47ee6b8624bea0ef538e35df7976e72c024b1e
Christos Deligkaris <deligkaris@users.noreply.github.com>
2026-02-05 08:41:08 -0500

    renamed partition size info function

--- utilities.py
+++ utilities.py
@@ -695,6 +695,6 @@ def add_ccn_from_pos(DF,posDF, providerZip="providerZip",providerName="providerN
 
     return DF
 
-def print_partition_sizes(df):
+def print_partition_size_info(df):
     print(df.withColumn("partID", F.spark_partition_id()).groupBy("partID").count().describe("count").show())
 
