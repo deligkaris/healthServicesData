60e8babe8cd59c2eb9cdb530b8cd3641bf895054
Christos Deligkaris <deligkaris@users.noreply.github.com>
2026-03-12 10:53:56 -0400

    add_orgnpinm_column_prior_year added in base

--- cms/base.py
+++ cms/base.py
@@ -2110,7 +2110,16 @@ def add_shortTermInpatientOrganization(baseDF):
                                        (F.col("psychiatricHospital")==0) & (F.col("ltcHospital")==0), F.lit(1)).otherwise(F.lit(0)))
     return baseDF
 
-    
+def add_orgnpinm_column_prior_year(baseDF, column="providerSepticShockVol"):
+    '''Adds the column's value from the prior year.
+    For some organizations the prior year will be 2 years prior because spark will use whatever row is lagging/behind the current one
+    so in that case I need to manually change the prior year quantity to null.'''
+    eachOrgnpinmYear = Window.partitionBy(["ORGNPINM","THRU_DT_YEAR"]).orderBy("THRU_DT_YEAR")
+    baseDF = (baseDF
+              .withColumn("priorThruYear", F.lag("THRU_DT_YEAR",1).over(eachOrgnpinmYear))
+              .withColumn(column+"PriorYear", F.lag(column,1).over(eachOrgnpinmYear))
+              .withColumn(column+"PriorYear", F.when( F.col("THRU_DT_YEAR")-F.col("priorThruYear")==1, F.col(column+"PriorYear")).otherwise(F.lit(None))))
+    return baseDF   
      
 
 
